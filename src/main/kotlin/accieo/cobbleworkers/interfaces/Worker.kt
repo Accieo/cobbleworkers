702d3993fbba7d6426767624073e99f143df99f2
Accieo <42718776+Accieo@users.noreply.github.com>
2025-06-27 18:47:52 +0200

    Add two new jobs

--- src/main/kotlin/accieo/cobbleworkers/interfaces/Worker.kt
+++ src/main/kotlin/accieo/cobbleworkers/interfaces/Worker.kt
@@ -13,6 +13,14 @@ import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
 
 interface Worker {
+    /**
+     * Determines if Pokémon is eligible to be a worker.
+     * NOTE: This is used to prevent running the tick method unnecessarily.
+     */
     fun shouldRun(pokemonEntity: PokemonEntity): Boolean
+    /**
+     * Main logic loop for the worker, executed each tick.
+     * NOTE: Origin refers to the pasture's block position.
+     */
     fun tick(world: World, origin: BlockPos, pokemonEntity: PokemonEntity)
 }
---
+++ src/main/kotlin/accieo/cobbleworkers/jobs/TumblestoneHarvester.kt
@@ -0,0 +1,16 @@
+package accieo.cobbleworkers.jobs
+
+import accieo.cobbleworkers.interfaces.Worker
+import com.cobblemon.mod.common.entity.pokemon.PokemonEntity
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
+
+object TumblestoneHarvester : Worker {
+    override fun shouldRun(pokemonEntity: PokemonEntity): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun tick(world: World, origin: BlockPos, pokemonEntity: PokemonEntity) {
+        TODO("Not yet implemented")
+    }
+}

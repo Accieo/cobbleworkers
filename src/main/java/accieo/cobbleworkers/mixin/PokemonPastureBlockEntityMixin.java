eabb288751798361445cee3dc5b02c53414785c5
Accieo <42718776+Accieo@users.noreply.github.com>
2025-07-01 19:44:37 +0200

    Gracefully handle world saving teardown

--- src/main/java/accieo/cobbleworkers/mixin/PokemonPastureBlockEntityMixin.java
+++ src/main/java/accieo/cobbleworkers/mixin/PokemonPastureBlockEntityMixin.java
@@ -17,40 +17,39 @@ import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Objects;
+
 @Mixin(PokemonPastureBlockEntity.class)
 public class PokemonPastureBlockEntityMixin {
-	@Unique
-	private static long lastErrorLogTime = 0;
-	@Unique
-	private static final long ERROR_LOG_CD_MS = 10_000;
-
 	@Inject(at = @At("TAIL"), method = "TICKER$lambda$14")
 	private static void init(World world, BlockPos blockPos, BlockState blockState, PokemonPastureBlockEntity pastureBlock, CallbackInfo ci) {
 		if (world.isClient) return;
 
-		pastureBlock.getTetheredPokemon().forEach(tethering -> {
-			Pokemon pokemon = tethering.getPokemon();
-			if (pokemon != null && !pokemon.isFainted()) {
-				PokemonEntity pokemonEntity = pokemon.getEntity();
-
-				if (pokemonEntity == null) return;
-
-				try {
-					WorkerDispatcher.INSTANCE.tickAll(world, blockPos, pokemonEntity);
-				} catch (Exception e) {
-					long now = System.currentTimeMillis();
-					if (now - lastErrorLogTime > ERROR_LOG_CD_MS) {
-						Cobbleworkers.logger.error("[Cobbleworkers]: Error while processing pasture mixin");
-						Cobbleworkers.logger.error(e.getMessage());
-						lastErrorLogTime = now;
+		pastureBlock.getTetheredPokemon().stream()
+				.filter(Objects::nonNull)
+				.forEach(tethering -> {
+					Pokemon pokemon;
+					try {
+						pokemon = tethering.getPokemon();
+					} catch (Exception e)  {
+						Cobbleworkers.logger.error("[Cobbleworkers] - Failed to get Pokémon from tethering: {}", e.getMessage());
+						return;
 					}
-				}
-			}
-		});
+
+					if (pokemon == null || pokemon.isFainted()) return;
+
+					PokemonEntity pokemonEntity = pokemon.getEntity();
+					if (pokemonEntity == null) return;
+
+					try {
+						WorkerDispatcher.INSTANCE.tickAll(world, blockPos, pokemonEntity);
+					} catch (Exception e) {
+						Cobbleworkers.logger.error("[Cobbleworkers] - Error processing WorkerDispatcher.tickAll {}", e.getMessage());
+					}
+				});
 	}
 }
